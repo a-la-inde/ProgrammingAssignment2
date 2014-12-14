bad536251f9360d4428153d5cd927556291b28b5
Alla <al.tyndik@gmail.com>
2014-12-14 13:29:56 +0300

    cachematrixChange

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,61 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+library("Matrix", lib.loc="C:/Program Files/R/R-3.1.0/library") 
+
+  
+## Write an object 
 
 makeCacheMatrix <- function(x = matrix()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() 
+    x
+  setsolve <- function(solve) 
+    m <<- solve
+  getsolve <- function() 
+    m
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
+  }
 
-}
 
-
-## Write a short comment describing this function
+## 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+    invx <- x$getsolve()
+    if(!is.null(invx)) {
+      message("getting cached data")
+      return(invx)
+    }
+    data <- x$get()
+    invx <- solve(data, ...)
+    x$setsolve(invx)
+    invx
+  }
+
+
+## check functions 
+
+m <- matrix(sample.int(100,size=16,replace=TRUE), nrow=4)
+x <- makeCacheMatrix(m)
+class(x)
+
+cacheSolve(x)
+
+y <- cacheSolve(x)
+
+class(y)
+
+m <- matrix(sample.int(100,size=25,replace=TRUE), nrow=5)
+x <- makeCacheMatrix(m)
+cacheSolve(x)
+
+cacheSolve(x)
+
+## check time using
+
+system.time(x <- matrix(data=NA,nrow=10000,ncol=10000)) 
+
